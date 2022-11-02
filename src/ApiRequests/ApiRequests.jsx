9ba12b16e6443dbdc3ad77d6c1b4ea45033ef6dc
Your Name <msyislam@gmail.com>
2022-11-02 20:24:59 +0400

    renamed folder ApiReq to utis

--- src/ApiRequests/ApiRequests.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const ApiRequests = {
-    getArticles() {
-        return axios.get(`https://news-app-msy.herokuapp.com/api/articles`);
-    }
-}
-
-export default ApiRequests;
