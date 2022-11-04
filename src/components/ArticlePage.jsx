e368085d2bfb401494b6bfbc07c6ab45de737d71
Your Name <msyislam@gmail.com>
2022-11-04 14:01:06 +0400

    imported HelperFunction.js and refactored code using it

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,10 +1,7 @@
 import { useParams } from "react-router-dom"
 import ApiRequests from "../Utils/ApiRequests";
 import { useState, useEffect } from "react";
-import imageCoding from '../Images/topic/imageCoding.png'
-import imageCooking from '../Images/topic/imageCooking.png'
-import imageFootball from '../Images/topic/imageFootball.png'
-import moment from "moment/moment";
+import HelperFunctions from "../Utils/HelperFunctions";
 
 
 const ArticlePage = () => {
@@ -21,19 +18,10 @@ const ArticlePage = () => {
 
     if(loading) return <h3>Loading...</h3>
     
-    const createdAt = moment.utc(article.created_at).format('DD/MM/YYYY')
-
     return  <>
         <h2 className = "articleTitle"> {article.title} </h2>
-        <h6> Posted by {article.author} on {createdAt}</h6>
-        {article.topic === 'coding' ? (
-            <img className="imagesOfTopic" src={imageCoding} alt="coding" />
-        ) : article.topic === 'cooking' ? (
-            <img className="imagesOfTopic" src={imageCooking} alt="cooking" />
-        ) : (
-            <img className="imagesOfTopic" src={imageFootball} alt="football" />
-        )}
-
+        <h6> Posted by {article.author} on {HelperFunctions.generateDate(article.created_at)}</h6>
+        {HelperFunctions.generateImageByTopic(article.topic)}
         <h4 className = "articleComment"> {article.body}</h4>
         <h4 className = "articleVotes" >{article.votes}</h4>
      </>
