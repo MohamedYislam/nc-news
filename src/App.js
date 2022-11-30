11f7ba228726a45f27cb04da316c7d30df036a4f
Your Name <msyislam@gmail.com>
2022-11-30 09:13:06 +0400

    Adding UserContext

--- src/App.js
+++ src/App.js
@@ -5,21 +5,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavigateTopic from './components/NavigateTopic';
 import Topics from './components/Topics';
 import ArticlePage from './components/ArticlePage';
+import { UserContext } from './Context/userContext';
 
 
 function App() {
   return (
-    <BrowserRouter> 
-      <div className="App">
-        <Header />
-        <NavigateTopic/>
-        <Routes>
-            <Route path = "/" element = {< Articles />} />
-            <Route path = "/topics/:topic" element = {< Topics /> } />
-            <Route path = "/articles/:article_id" element = {< ArticlePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <UserContext.Provider value={{ username: "grumpy19" }}>
+      <BrowserRouter> 
+        <div className="App">
+          <Header />
+          <NavigateTopic/>
+          <Routes>
+              <Route path = "/" element = {< Articles />} />
+              <Route path = "/topics/:topic" element = {<Topics />} />
+              <Route path = "/articles/:article_id" element = {< ArticlePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </UserContext.Provider>
+
   );
 }
 
