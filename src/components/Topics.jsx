d9ae710e77f9a4e47a22d4f87b1477f3f14a6fbe
Your Name <msyislam@gmail.com>
2022-12-03 10:33:14 +0400

    Added error handling for invalid Topic paths

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -7,6 +7,7 @@ const Topics = () => {
     const { topic } = useParams();
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState([true])
+    const [error, setError] = useState(false)
 
     useEffect(() => {
         ApiRequests.getArticlesByTopic(topic)
@@ -14,9 +15,14 @@ const Topics = () => {
                 setArticles(filteredArticles)
                 setLoading(false)
             })
+            .catch((res) => {
+                setLoading(false)
+                setError(true)
+            })
     }, [topic])
 
     if (loading) return <h5> Loading... </h5>
+    if (error) return <h2 className="errorClass"> Invalid Topic </h2>
 
     return  <ul className = "articles"> 
         {articles.map((article) => {
