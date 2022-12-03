00e23ff5bfd6cc991355c3458d1cf4c690fcd179
Your Name <msyislam@gmail.com>
2022-12-03 10:34:28 +0400

    refactoring

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,9 +1,10 @@
 import HelperFunctions from "../Utils/HelperFunctions"
 import ApiRequests from "../Utils/ApiRequests"
 import { UserContext } from '../Context/userContext';
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const CommentCard = (props) => {
+    const [button, setButton] = useState(false)
     const { username } = useContext(UserContext);
     const { comment : { author, body, created_at, comment_id, votes }} = props
 
@@ -11,16 +12,17 @@ const CommentCard = (props) => {
         console.log(comment_id, "<<<comment_id")
         ApiRequests.removeComment(comment_id)
             .then(() => {
+                setButton(false)
                 window.location.reload(false);
             })
     }
-
+    
     return <div className = "commentCardClass"> 
                 <p>  {body} </p>
                 <h4> {votes} </h4>
                 <h5> {author} on {HelperFunctions.generateDate(created_at)}</h5>
                 {author === username ? (
-                    <button onClick = {handleDelete}>Delete Comment</button>
+                    <button onClick = {handleDelete} disabled={button}>Delete Comment</button>
                 ) :  (
                     <></>
                 )}
